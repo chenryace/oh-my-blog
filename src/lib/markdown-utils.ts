9388a427ea5f6e6bde06b81bcc969537a70733a2
qfdk <qfdk2010@gmail.com>
2024-11-21 23:04:55 +0100

    fix: css 代码高亮

--- src/lib/markdown-utils.ts
+++ src/lib/markdown-utils.ts
@@ -1,13 +1,13 @@
 import type MarkdownIt from "markdown-it";
-import hljs from 'highlight.js';
+import hljs from "highlight.js";
 // 预加载你需要的语言
-import 'highlight.js/lib/languages/java';
-import 'highlight.js/lib/languages/javascript';
-import 'highlight.js/lib/languages/bash';
-import 'highlight.js/lib/languages/shell';
+import "highlight.js/lib/languages/java";
+import "highlight.js/lib/languages/javascript";
+import "highlight.js/lib/languages/bash";
+import "highlight.js/lib/languages/shell";
 
 export const createMarkdownParser = async (): Promise<MarkdownIt> => {
-    const MarkdownIt = (await import('markdown-it')).default;
+    const MarkdownIt = (await import("markdown-it")).default;
 
     const parser = new MarkdownIt({
         html: true,
@@ -20,9 +20,11 @@ export const createMarkdownParser = async (): Promise<MarkdownIt> => {
                         language: lang,
                         ignoreIllegals: true
                     }).value;
-                } catch (__) {}
+                } catch (e) {
+                    console.log(e);
+                }
             }
-            return ''; // 使用默认的转义
+            return ""; // 使用默认的转义
         }
     });
 
